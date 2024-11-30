1381fdc561a10a7e71d7ff17ca1ad21c79333c30
to-typ <ben.kutter@gmx.de>
2024-11-30 02:26:47 +0100

    a mobile update

--- app/credits/page.js
+++ app/credits/page.js
@@ -6,17 +6,17 @@ export default function Credits() {
       <div className="text-2xl items-center justify-center float-left">
         <div className="text-center">
           <p className="inline-block ">ORIGINAL-THEME</p>
-          <p className="inline-block text-8xl">
+          <p className="inline-block sm:text-8xl text-7xl">
             <a href="https://uso.kkx.one/style/174975">@INTRES</a>
           </p>
           <p className="inline-block ">SAFARI-APP</p>
-          <p className="inline-block text-8xl">
+          <p className="inline-block sm:text-8xl text-7xl">
             <a href="https://github.com/Momixxxxxxx">MORITZ </a>
           </p>
         </div>
         <div className="text-center">
           <p className="inline-block ">PAGE&THEME</p>
-          <p className="inline-block text-8xl">
+          <p className="inline-block sm:text-8xl text-7xl">
             <a href="https://github.com/to-typ">BEN </a>
           </p>
         </div>
