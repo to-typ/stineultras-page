5801645b7279b8874b44f0a313c3804438106231
to-typ <ben.kutter@gmx.de>
2024-11-30 13:06:34 +0100

    another mobile update

--- app/page.js
+++ app/page.js
@@ -22,8 +22,8 @@ export default function Home() {
         <>
           <NoInstruction>
             <p>
-              Nicht für Chrome auf Mobilen Geräten verfügbar, da es
-              keine Browser Extensions in Chrome auf Mobilgeräten gibt.
+              Nicht für Chrome auf Mobilen Geräten verfügbar, da es keine
+              Browser Extensions in Chrome auf Mobilgeräten gibt.
             </p>
           </NoInstruction>
         </>
@@ -35,8 +35,8 @@ export default function Home() {
 
   return (
     <div>
-      <div className="grid sm:grid-rows-[5rem_1fr_100px] grid-rows-[10rem_17rem_100px] grid-cols-[1fr_2fr_1fr] items-center justify-items-center min-h-screen pb-9 gap-0 bg-white text-[#025392] font-[family-name:oswald]">
-        <div className="fixed sm:w-full w-[110%] sm:border-y-0 border-y-8 border-[#025392] sm:top-0 top-[12rem] bg-white row-start-1 row-end-3 row-start-2 col-start-1 col-end-3 ">
+      <div className="grid sm:grid-rows-[5rem_1fr_100px] grid-rows-[10rem_1.4rem_10rem] grid-cols-[1fr_2fr_1fr] items-center justify-items-center min-h-screen pb-9 gap-0 bg-white text-[#025392] font-[family-name:oswald]">
+        <div className="fixed sm:w-full w-[110%] sm:border-y-0 border-y-8 border-[#025392] sm:top-0 top-[16rem] bg-white row-start-1 row-end-3 row-start-2 col-start-1 col-end-3 ">
           <Comparison />
         </div>
         <div className="flex flex-col sm:row-start-2 row-start-1 col-start-2 w-full items-center text-center sticky sm:top-10 top-2 mb-16 drop-shadow-[0_0_10px_rgba(99,99,99,0.8)] z-10">
@@ -51,7 +51,7 @@ export default function Home() {
           <br></br>
         </div>
         <main className="flex flex-col w-full items-center justify-center gap-0 row-start-1 col-start-2">
-          <div className="sm:top-[90%] top-[66%] bg-gradient-to-b from-[#025392] to-[#0271bb] rounded-md border-2 border-[#025392] left-0 w-full h-auto text-center items-center justify-center z-30 absolute p-1.5 text-blue-50 text-black">
+          <div className="sm:top-[90%] top-[55%] bg-gradient-to-b from-[#025392] to-[#0271bb] rounded-md border-2 border-[#025392] left-0 w-full h-auto text-center items-center justify-center z-30 absolute p-1.5 text-blue-50 text-black">
             <h1 className="m-4 text-3xl font-extrabold">Anleitung:</h1>
             <div>
               <button
@@ -98,9 +98,10 @@ export default function Home() {
             </div>
           </div>
         </main>
-        <div className="row-start-3 col-start-1 col-end-4 w-full flex justify-center">
-          <a className="flex relative text-4xl font-[600] text-center z-10 drop-shadow-[0_0_10px_rgba(99,99,99,0.8)] ">
-            Stine ist scheiße. Mach es dir zumindest ein bisschen hübscher.
+        <div className="row-start-3 col-start-1 col-end-4 w-full h-full sm:h-auto flex justify-center">
+          <a className="flex relative sm:text-4xl text-xl font-[600]  text-center z-10 drop-shadow-[0_0_10px_rgba(99,99,99,0.8)]">
+            Stine ist scheiße.<br className="sm:hidden"></br> Mach es dir
+            zumindest ein bisschen hübscher.
           </a>
         </div>
       </div>
