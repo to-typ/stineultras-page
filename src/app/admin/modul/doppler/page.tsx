80e26462668125cd1ed590fd1563f09f25307fae
to-typ <ben.kutter@gmx.de>
2026-03-05 18:09:45 +0100

    remove doubles

---
+++ src/app/admin/modul/doppler/page.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/ui/spinner";
+import { Card, CardTitle } from "@/components/ui/card";
+import { Check, Trash, Info } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+
+interface Veranstaltung {
+  id: number;
+  veranstaltung: {
+    name: string;
+  };
+}
+
+interface Modul {
+  id: number;
+  name: string;
+  veranstaltungen: Veranstaltung[];
+}
+
+export default function Admin() {
+  const [modulId, setModulId] = useState<number>(0);
+  const [modulData, setModulData] = useState<Modul | null>(null);
+  const [matches, setMatches] = useState<Modul[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fetchNextId() {
+      if (modulData == null) {
+        setLoading(true);
+        const response = await fetch(`/api/admin/moduls?next=${modulId}`, {
+          method: "GET",
+        });
+        const nextId = await response.json();
+        setModulId(nextId.id ? nextId.id : modulId + 1);
+      } else {
+        setLoading(true);
+        fetch(`/api/admin/moduls?name=${modulData.name}`, {
+          method: "GET",
+        })
+          .then((res) => res.json())
+          .then((data) => data.matches)
+          .then((data) => setMatches(Array.isArray(data) ? data : [data]))
+          .then(() => setLoading(false))
+          .catch(() => {
+            setLoading(false);
+          });
+      }
+    }
+    fetchNextId();
+  }, [modulData]);
+
+  useEffect(() => {
+    if (modulId === 0) return;
+    setLoading(true);
+    fetch(`/api/admin/moduls?id=${modulId}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => setModulData(Array.isArray(data) ? data[0] : data))
+      .then(() => setLoading(false))
+      .catch(() => {
+        setLoading(false);
+      });
+  }, [modulId]);
+
+  const handleDelete = (id: number) => {
+    fetch(`/api/admin/moduls?id=${id}`, {
+      method: "DELETE",
+    });
+  };
+
+  const handleNext = () => {
+    setModulId(modulId + 1);
+  };
+
+  return (
+    <>
+      <div className="text-white flex flex-col m-8 gap-6 h-fit justify-center">
+        <Input
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              setModulId(Number((e.target as HTMLInputElement).value));
+            }
+          }}
+          placeholder="Modul ID"
+        />
+        {modulData && (
+          <div className="w-full items-center justify-center flex flex-col gap-4">
+            <Card className="p-4 w-1/3 flex flex-row justify-between">
+              <div>
+                <CardTitle>{modulData.name}</CardTitle>
+                <p>Anzahl Veranstaltungen: {modulData.veranstaltungen.length}</p>
+                <p>ID: {modulData.id}</p>
+              </div>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button variant="outline">
+                    <Info className="h-5 w-5" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-90">
+                  {modulData.veranstaltungen.map((veranstaltung: Veranstaltung) => (
+                    <p className="py-1" key={veranstaltung.id}>
+                      {veranstaltung.veranstaltung.name}
+                    </p>
+                  ))}
+                </PopoverContent>
+              </Popover>
+            </Card>
+            <Separator />
+            {matches.length > 0 &&
+              matches.map((match) => (
+                <Card key={match.id} className="p-4 w-1/3 flex flex-row justify-between">
+                  <div>
+                    <CardTitle>{match.name}</CardTitle>
+                    <p>Anzahl Veranstaltungen: {match.veranstaltungen.length}</p>
+                    <p>ID: {match.id}</p>
+                  </div>
+                  <div className="w-min gap-2 flex flex-col">
+                    <Button variant="destructive" onClick={() => handleDelete(match.id)}>
+                      <Trash className="h-5 w-5" />
+                    </Button>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button variant="outline">
+                          <Info className="h-5 w-5" />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-90">
+                        {modulData.veranstaltungen.map((veranstaltung: Veranstaltung) => (
+                          <p className="py-1" key={veranstaltung.id}>
+                            {veranstaltung.veranstaltung.name}
+                          </p>
+                        ))}
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                </Card>
+              ))}
+          </div>
+        )}
+        {!modulData && !loading && (
+          <div className="w-full items-center justify-center flex">
+            <p className="w-full text-center">Keine Daten gefunden</p>
+          </div>
+        )}
+        {loading && (
+          <>
+            <Label className="w-full text-center flex items-center justify-center gap-2">
+              Loading <Spinner />
+            </Label>
+          </>
+        )}
+        <div className="flex flex-row gap-4 h-full">
+          <Button onClick={handleNext} className="bg-green-500 w-full h-20">
+            <Check style={{ width: 64, height: 64 }} />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/api/admin/moduls/route.ts
+++ src/app/api/admin/moduls/route.ts
@@ -1,110 +1,130 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const stringSimilarity = require("string-similarity");
 
 const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
-    const id = req.nextUrl.searchParams.get("id");
-    if (!id) {
-        const search = req.nextUrl.searchParams.get("search");
-        if (!search) {
-            const next = req.nextUrl.searchParams.get("next");
-            const moduls = await prisma.modul.findMany({orderBy: { id: "asc" }});
-            if (next) {
-                const nextId = parseInt(next);
-                const nextModul = moduls.find((modul) => modul.id > nextId);
-                return NextResponse.json(
-                    {id: nextModul?.id}, { status: 200 }
-                );  
-            }
-            return NextResponse.json(
-                {id: moduls[0]?.id}, { status: 200 }
-            )
-        }
-        return NextResponse.json(
-            await prisma.modul.findMany({
-                where: {
-                    name: {
-                        contains: search,
-                        mode: "insensitive",
-                    }
-                },
-                include: {
-                    veranstaltungen: {
-                        include: {
-                            veranstaltung: true,
-                        },
-                    },
-                },
-            })
-        );
-    }
-    const modul = await prisma.modul.findUnique({
-        where: { id: Number(id) },
-        include: {
-            veranstaltungen: {
-                include: {
-                    veranstaltung: true,
-                },
-            },
+  const name = req.nextUrl.searchParams.get("name");
+  if (name) {
+    const allModuls = await prisma.modul.findMany();
+    const matches = allModuls.filter(
+      (m) => stringSimilarity.compareTwoStrings(m.name.toLowerCase(), name.toLowerCase()) > 0.8,
+    );
+    const fullMatches = await prisma.modul.findMany({
+      where: {
+        id: {
+          in: matches.map((m) => m.id),
         },
+      },
+      include: {
+        veranstaltungen: {
+          include: {
+            veranstaltung: true,
+          },
+        },
+      },
     });
-    return NextResponse.json(modul);
+    return NextResponse.json({ matches: fullMatches }, { status: 200 });
+  }
+  const id = req.nextUrl.searchParams.get("id");
+  if (!id) {
+    const search = req.nextUrl.searchParams.get("search");
+    if (!search) {
+      const next = req.nextUrl.searchParams.get("next");
+      const moduls = await prisma.modul.findMany({ orderBy: { id: "asc" } });
+      if (next) {
+        const nextId = parseInt(next);
+        const nextModul = moduls.find((modul) => modul.id > nextId);
+        return NextResponse.json({ id: nextModul?.id }, { status: 200 });
+      }
+      return NextResponse.json({ id: moduls[0]?.id }, { status: 200 });
+    }
+    return NextResponse.json(
+      await prisma.modul.findMany({
+        where: {
+          name: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+        include: {
+          veranstaltungen: {
+            include: {
+              veranstaltung: true,
+            },
+          },
+        },
+      }),
+    );
+  }
+  const modul = await prisma.modul.findUnique({
+    where: { id: Number(id) },
+    include: {
+      veranstaltungen: {
+        include: {
+          veranstaltung: true,
+        },
+      },
+    },
+  });
+  return NextResponse.json(modul);
 }
 
 export async function DELETE(req: NextRequest) {
-    const empty = req.nextUrl.searchParams.get("empty");
-    if (empty) {
-        const emptyModuls = await prisma.modul.findMany({
-            where: {
-                veranstaltungen: {
-                    none: {},
-                },
-            },
-        });
-        const emptyModulIds = emptyModuls.map((modul) => modul.id);
-        return NextResponse.json(
-            await prisma.modul.deleteMany({
-                where: {
-                    id: {
-                        in: emptyModulIds,
-                    },
-                },
-            })
-        );
+  const empty = req.nextUrl.searchParams.get("empty");
+  if (empty) {
+    const emptyModuls = await prisma.modul.findMany({
+      where: {
+        veranstaltungen: {
+          none: {},
+        },
+      },
+    });
+    const emptyModulIds = emptyModuls.map((modul) => modul.id);
+    return NextResponse.json(
+      await prisma.modul.deleteMany({
+        where: {
+          id: {
+            in: emptyModulIds,
+          },
+        },
+      }),
+    );
+  }
+  const id = req.nextUrl.searchParams.get("id");
+  if (!id) {
+    const ids = req.nextUrl.searchParams.get("ids");
+    if (!ids) {
+      return NextResponse.json({ error: "id or ids parameter is required" }, { status: 400 });
     }
-    const id = req.nextUrl.searchParams.get("id");
-    if (!id) {
-        const ids = req.nextUrl.searchParams.get("ids");
-        if (!ids) {
-            return NextResponse.json({ error: "id or ids parameter is required" }, { status: 400 });
-        }
 
-        const idArray = ids.split(",").map((id) => Number(id.trim()));
-
-        await prisma.veranstaltungInModul.deleteMany({
-            where: {
-                modulId: {
-                    in: idArray,
-                },
-            },
-        });
-        await prisma.modul.deleteMany({
-            where: {
-                id: {
-                    in: idArray,
-                },
-            },
-        });
-        return NextResponse.json({ success: true, deletedIds: idArray });
-    }
+    const idArray = ids.split(",").map((id) => Number(id.trim()));
 
     await prisma.veranstaltungInModul.deleteMany({
-        where: { modulId: Number(id) },
+      where: {
+        modulId: {
+          in: idArray,
+        },
+      },
     });
-    return NextResponse.json(
-        await prisma.modul.delete({
-            where: { id: Number(id) },
-        }),
-    );
+    await prisma.modul.deleteMany({
+      where: {
+        id: {
+          in: idArray,
+        },
+      },
+    });
+    return NextResponse.json({ success: true, deletedIds: idArray });
+  }
+
+  await prisma.veranstaltungInModul.deleteMany({
+    where: { modulId: Number(id) },
+  });
+  return NextResponse.json(
+    await prisma.modul.delete({
+      where: { id: Number(id) },
+    }),
+  );
 }
